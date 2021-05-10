57c442908886a19fa482292fb072fae7461dfd55
tomj <66176+tomj@users.noreply.github.com>
2021-05-10 20:47:21 +0800

    Add LEASH

--- hordak/defaults.py
+++ hordak/defaults.py
@@ -19,13 +19,14 @@ CURRENCY_CODE_MAX_LENGTH = 20
 class Setup:
     @staticmethod
     def add_custom_currencies():
-        moneyed.add_currency("SHIB", "000", "Shiba Token", None)
-        moneyed.add_currency("ETH", "000", "Ethereum", None)
-        moneyed.add_currency("DAI", "000", "Makerdao", None)
         moneyed.add_currency("ADA", "000", "Cardano", None)
-        moneyed.add_currency("NANO", "000", "Nano", None)
-        moneyed.add_currency("USDT", "000", "Tether", None)
-        moneyed.add_currency("COMP", "000", "Compound", None)
-        moneyed.add_currency("MATIC", "000", "Polygon", None)
         moneyed.add_currency("BTC", "000", "Bitcoin", None)
-        moneyed.add_currency("KRILL", "000", "Polywhale", None)
+        moneyed.add_currency("COMP", "000", "Compound", None)
+        moneyed.add_currency("DAI", "000", "Dai", None)
+        moneyed.add_currency("ETH", "000", "Ethereum", None)
+        moneyed.add_currency("KRILL", "000", "Krill", None)
+        moneyed.add_currency("LEASH", "000", "Leash", None)
+        moneyed.add_currency("MATIC", "000", "Polygon", None)
+        moneyed.add_currency("NANO", "000", "Nano", None)
+        moneyed.add_currency("SHIB", "000", "Shiba Token", None)
+        moneyed.add_currency("USDT", "000", "Tether", None)
